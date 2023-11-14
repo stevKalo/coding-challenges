cf67db0b0ab14bf99b916381e6e2df010e8a1452
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-11-14 12:08:27 -0500

    Complete coding challenge for 11.14.2023

---
+++ challenges/2023.11.14/app.js
@@ -0,0 +1,15 @@
+// Instructions:
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Parameters: name and owner args
+
+// Return: custom greeting
+
+// Examples:
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+// Pseudocode:
+function greet(name, owner) {
+  return name == owner ? "Hello boss" : "Hello guest";
+}
