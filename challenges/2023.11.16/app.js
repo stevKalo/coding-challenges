840d1f914700680cd6e0f9dc571d06390936e43f
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-11-16 22:33:55 -0500

    Complete coding challenge for 11.16.2023

---
+++ challenges/2023.11.16/app.js
@@ -0,0 +1,17 @@
+// Instructions:
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+// The binary number returned should be a string.
+
+// Parameters: 2 numbers
+
+// Return: sum of numbers in binary
+
+// Examples:
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+// Pseudocode:
+function addBinary(a, b) {
+  let sum = a + b;
+  return sum.toString(2);
+}
