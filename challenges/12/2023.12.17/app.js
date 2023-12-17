6b65cf88700d226ead36e80e9cbfe4f9007f61c4
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-12-17 11:58:18 -0500

    Complete coding challenge for 12.17.2023

---
+++ challenges/12/2023.12.17/app.js
@@ -0,0 +1,21 @@
+// Instructions:
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Parameters:
+
+// Return:
+
+// Examples:
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+
+// Pseudocode:
+const binaryArrayToNumber = (arr) => {
+  return parseInt(arr.join(""), 2);
+};
