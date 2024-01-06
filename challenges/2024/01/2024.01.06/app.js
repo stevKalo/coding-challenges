fbc71fdb9257ba5a818eba48ca904d554763ca5f
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2024-01-06 18:11:27 -0500

    Complete coding challenge for 01.06.2024

---
+++ challenges/2024/01/2024.01.06/app.js
@@ -0,0 +1,18 @@
+// Instructions:
+// In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+
+// Parameters: integer value and integer limit
+
+// Return: array multiples of value up to and including limit
+
+// Examples:
+// For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+
+// Pseudocode:
+function findMultiples(num, limit) {
+    let arr = []
+for (let i = num ; i <= limit ; i += num) {
+    arr.push(i)
+}
+    return arr
+}
