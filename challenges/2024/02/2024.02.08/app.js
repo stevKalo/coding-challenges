f50793970dda846c2335f05f7b643d6226f4ecff
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2024-02-08 13:02:01 -0500

    Complete coding challenge for 02.08.2024

---
+++ challenges/2024/02/2024.02.08/app.js
@@ -0,0 +1,17 @@
+// Instructions:
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+// Can you help me fix the function?
+
+// Parameters: single integer
+
+// Return: integer + 5
+
+// Examples:
+// 5 => 10
+// 7 => 12
+
+// Pseudocode:
+function addFive(num) {
+  const total = num + 5;
+  return total;
+}
